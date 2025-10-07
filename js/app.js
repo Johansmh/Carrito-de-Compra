d475db14ccb43f6d14c089b1eba7c7b4127ac239
Johansmh  <johansaidmh1@gmail.com>
2025-10-07 17:04:22 -0500

    Se crea un objeto con el contenido del producto al que le dimos click

--- js/app.js
+++ js/app.js
@@ -21,6 +21,21 @@ function agregarProducto(e) {
     e.preventDefault()
     if(e.target.classList.contains('agregar-carrito')) {
         const productoSeleccionado = e.target.parentElement.parentElement
-        console.log(productoSeleccionado)
+        leerDatosProducto(productoSeleccionado);
     }
+}
+
+// 4. Leer el contenido del HTML al que le dimos click y extrae la información del producto
+function leerDatosProducto(producto) {
+    console.log(curso)
+
+    // Crear un objeto con el contenido del producto actual
+    const infoProducto = {
+        imagen: producto.querySelector('img').src,
+        titulo: producto.querySelector('h3').textContent,
+        precio_: producto.querySelector('.precio').textContent,
+        id: producto.querySelector('a').getAttribute('data-id'), 
+        cantidad: 1
+    }
+    console.log(infoProducto)
 }
