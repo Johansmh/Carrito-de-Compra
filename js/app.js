785bc840c4f3d09c24ba69ed007dcd253b4ba8a3
Johansmh  <johansaidmh1@gmail.com>
2025-10-07 15:48:00 -0500

    Se definen las variables en el js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
+//1. Variables
 
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody')
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito')
+const contenido = document.querySelector('#listado-productos')
